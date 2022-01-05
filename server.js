a5f4d1dc690786026b7d01fac4304d887f5807f1
Zeinab Farag <zeinabfarag454@gmail.com>
2022-01-05 00:57:29 -0500

    cors

--- server.js
+++ server.js
@@ -22,6 +22,30 @@ mongoose
 mongoose.Promise = global.Promise;
 app.use(cookieParser());
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_APP_URL);
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.use(express.json());
 
 app.use(
