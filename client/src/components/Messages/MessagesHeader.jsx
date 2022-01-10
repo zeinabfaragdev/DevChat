71e0626c2e5bd0e7cf2d76d7ca57fdadcd467a84
Zeinab Farag <zeinabfarag454@gmail.com>
2022-01-09 19:09:43 -0500

    add number of users

--- client/src/components/Messages/MessagesHeader.jsx
+++ client/src/components/Messages/MessagesHeader.jsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { Header, Segment, Input, Icon } from "semantic-ui-react";
-
+import { useSelector } from "react-redux";
 const MessagesHeader = () => {
+  const currentChannel = useSelector((state) => state.channel.current);
+  const messages = useSelector(
+    (state) => state.channel.current && state.channel.current.messages
+  );
+
+  let numUsers = "";
+
+  const calcNumUsers = () => {
+    let uniqueUsers = [...new Set(messages.map((item) => item.user._id))];
+    if (uniqueUsers.length === 0) {
+      numUsers = "No Users";
+    } else if (uniqueUsers.length === 1) {
+      numUsers = "1 User";
+    } else {
+      numUsers = `${uniqueUsers.length} Users`;
+    }
+  };
+
+  messages && calcNumUsers();
+
   return (
     <Segment clearing>
       <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
         <span>
-          Channel
+          #{currentChannel.name}
           <Icon name="star outline" color="black" />
         </span>
-        <Header.Subheader>2 Users</Header.Subheader>
+        <Header.Subheader>{numUsers}</Header.Subheader>
       </Header>
       <Header floated="right">
         <Input
